d5d4c1997e8882612eaaad5f56e4fe4caaa1300b
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 13:31:06 -0400

    Add files via upload

---
+++ VMWare Find Old Snapshots.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module VMware.VimAutomation.Cis.Core
+Import-Module VMware.VimAutomation.Common
+Import-Module VMware.VimAutomation.Core
+#Age in days of snapshots to list
+( $Age = 2 )
+
+Connect-VIServer vcenter.corp.com
+$vm = Get-VM
+$snapshots = Get-Snapshot -VM $vm
+Write-Host -ForegroundColor Red "Old snapshots found:"
+foreach ( $snap in $snapshots ) {
+    if ( $snap.Created -lt (Get-Date).AddDays( -$Age ) ) {
+            Write-Host "Name: " $snap.VM "  Size: " $snap.SizeGB "  Created: " $snap.Created
+    }
+}
